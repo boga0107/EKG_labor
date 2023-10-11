11eeac509231bebb2bb88df99e7d53d5a5585b2b
boga0107 <gabriel.boehm11@gmail.com>
2023-10-11 14:30:48 +0200

    adjustments

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*
 Projekt: EKG-Labor
 Aufagenstellung:
-Ziel des Labors ist der Entwurf einer Signalverarbeitungskette in einem Mikrocontroller anhand eines EKG-Rekorders. 
+Ziel des Labors ist der Entwurf einer Signalverarbeitungskette in einem Mikrocontroller anhand eines EKG-Rekorders.
 Hierzu sollen EKG-Daten aufgenommen und über WLAN an einen PC übertragen werden. Die Verarbeitung der Daten findet teilweise
 im Controller und teilweise auf dem PC in MATLAB statt.
 
@@ -24,14 +24,15 @@ uint8_t posX = 0;
 SSD1306Wire myDisplay(0x3c, SDA, SCL);
 
 /* Sonstige Variablen */
-bool flagRead = false;      // Scheduling - Timer-Interrupt
+bool flagRead = false; // Scheduling - Timer-Interrupt
 float sinusValue = 0;
-uint8_t freque = 1;         // Frequenz vom Sinus, aktuell 1Hz       
+uint8_t freque = 1; // Frequenz vom Sinus, aktuell 1Hz
 uint8_t dacValue = 0;
 uint16_t adcValue = 0;
+uint16_t adcValueOld = 0;
 
 /* Klassen */
-TaskHandle_t TaskWriteSinus;      // Task
+TaskHandle_t TaskWriteSinus; // Task
 
 /* Funktionen */
 void setup();
@@ -60,13 +61,22 @@ void loop()
   {
     adcValue = analogRead(33);
     adcValue = map(adcValue, 0, 4095, 55, 10);
-    myDisplay.setPixel(posX, adcValue);
+
+    if (posX == 0)
+    {
+      myDisplay.setPixel(posX, adcValue);
+    }
+    else
+    {
+      myDisplay.drawLine(posX - 1, adcValueOld, posX, adcValue);
+    }
     myDisplay.display();
+    adcValueOld = adcValue;
     posX++;
     flagRead = false;
   }
   /* Nach 2,5sek wird der Display zurück gesetzt */
-  if (posX == 128)
+  if (posX >= 128)
   {
     posX = 0;
     myDisplay.clear();
@@ -84,7 +94,7 @@ void timerInit()
 void IRAM_ATTR onTimer()
 {
   counterms++;
-  if (counterms % 20 == 0)
+  if (counterms % 18 == 0)
   {
     flagRead = true;
   }
@@ -95,8 +105,9 @@ void writeSinus(void *parameter)
   for (;;)
   {
     /* Sinus, 1Hz */
-    sinusValue = sin(2.0 * PI * freque * millis() / 1000.0);
+    sinusValue = sin(2.0 * PI * freque * counterms / 1000.0);
     dacValue = int(sinusValue * 127.0 + 127.0);
     dacWrite(25, dacValue);
+    delay(10);
   }
 }
