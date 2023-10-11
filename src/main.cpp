fbac45ab59a1faa08eea78c16ecb4de870cfcad1
1-WaltDisney <walter.orlov@gmx.de>
2023-10-11 13:29:01 +0200

    Kommentare und Ordnung

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,39 @@
+/*
+Projekt: EKG-Labor
+Aufagenstellung:
+Ziel des Labors ist der Entwurf einer Signalverarbeitungskette in einem Mikrocontroller anhand eines EKG-Rekorders. 
+Hierzu sollen EKG-Daten aufgenommen und über WLAN an einen PC übertragen werden. Die Verarbeitung der Daten findet teilweise
+im Controller und teilweise auf dem PC in MATLAB statt.
+
+Autoren: Gabriel Boehm & Walter Orlov
+Erstellt am 09.10.2023
+*/
+
+/* Bibliotheken */
 #include <Arduino.h>
 #include <math.h>
 #include "Wire.h"
 #include "SSD1306Wire.h"
 
-TaskHandle_t TaskWriteSinus;
-
+/* Variablen - Timer */
 hw_timer_s *timer = NULL;
 uint32_t counterms = 0;
 
-bool flagRead = false;
-float sinusValue = 0;
-uint8_t dacValue = 0;
-uint16_t adcValue = 0;
+/* Variablen - Display*/
 uint8_t posX = 0;
 SSD1306Wire myDisplay(0x3c, SDA, SCL);
 
+/* Sonstige Variablen */
+bool flagRead = false;      // Scheduling - Timer-Interrupt
+float sinusValue = 0;
+uint8_t freque = 1;         // Frequenz vom Sinus, aktuell 1Hz       
+uint8_t dacValue = 0;
+uint16_t adcValue = 0;
+
+/* Klassen */
+TaskHandle_t TaskWriteSinus;      // Task
+
+/* Funktionen */
 void setup();
 void loop();
 void timerInit();
@@ -24,23 +43,19 @@ void writeSinus(void *parameter);
 void setup()
 {
   timerInit();
+
   Serial.begin(115200);
   analogReadResolution(12);
-
-  xTaskCreate(
-      writeSinus,
-      "Write Sinus",
-      1000,
-      NULL,
-      1,
-      &TaskWriteSinus);
-
   myDisplay.init();
   myDisplay.flipScreenVertically();
+
+  xTaskCreate(writeSinus, "Write Sinus", 1000, NULL, 1, &TaskWriteSinus);
 }
 
+/* Hauptprogramm - Loop */
 void loop()
 {
+  /* Alle 20ms wird ein Pixel erstellt. */
   if (flagRead)
   {
     adcValue = analogRead(33);
@@ -50,6 +65,7 @@ void loop()
     posX++;
     flagRead = false;
   }
+  /* Nach 2,5sek wird der Display zurück gesetzt */
   if (posX == 128)
   {
     posX = 0;
@@ -76,13 +92,11 @@ void IRAM_ATTR onTimer()
 
 void writeSinus(void *parameter)
 {
-  bool plusMinus = true;
-  uint16_t period = 1000;
   for (;;)
   {
-    sinusValue = sin(2.0 * PI / float(period) * millis());
+    /* Sinus, 1Hz */
+    sinusValue = sin(2.0 * PI * freque * millis() / 1000.0);
     dacValue = int(sinusValue * 127.0 + 127.0);
     dacWrite(25, dacValue);
-    //vTaskDelay(10 / portTICK_PERIOD_MS);
   }
 }
