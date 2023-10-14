fc1290d8cd8e6eb25e1a27ee953f996c93cd0963
1-WaltDisney <walter.orlov@gmx.de>
2023-10-14 12:15:01 +0200

    UDP-Port implementiert

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@ Erstellt am 09.10.2023
 #include "SSD1306Wire.h"
 #include "readEKG.h"
 #include "Wifi.h"
+#include "WiFiUdp.h"
+#include "ESPAsyncWebServer.h"
 
 #define EKG_SAMPLING_TIME 100
 
@@ -33,12 +35,14 @@ float sinusValue = 0;
 uint8_t freque = 1; // Frequenz vom Sinus, aktuell 1Hz
 uint8_t dacValue = 0;
 uint16_t adcValue = 0;
+int remotePort = 0;
 // const char *wifiSSID = "FRITZ!Box 7590 VL";
 // const char *wifiPAS = "56616967766283031728";
-const char *wifiSSID = "Pixel7";
-const char *wifiPAS = "11223GB0107";
+const char *wifiSSID = "Vodafone-A1BC";
+const char *wifiPAS = "FNRaMbEtnTZGsn9C";
 String wifiCon = "Connecting to WiFi";
 IPAddress localIP;
+WiFiUDP Udp;
 
 /* Klassen */
 TaskHandle_t TaskWriteSinus; // Task
@@ -85,6 +89,7 @@ void loop()
   // }
 }
 
+
 void timerInit()
 {
   timer = timerBegin(0, 80, true);
@@ -108,18 +113,20 @@ void IRAM_ATTR onTimer()
 
 void wifiInit()
 {
-  myDisplay.clear();
   myDisplay.setTextAlignment(TEXT_ALIGN_CENTER);
 
   uint8_t indexWait = 0;
   uint8_t indexDot = 0;
 
+  /* WiFi-Mode:  */
   WiFi.mode(WIFI_STA);
   WiFi.begin(wifiSSID, wifiPAS);
 
+  /* DoWhile-Schleife um 30sec versuchen die WiFi Verbindung aufzubauen */
   do
   {
     myDisplay.clear();
+
     if (indexDot == 4)
     {
       wifiCon = "Connecting to WiFi";
@@ -134,31 +141,48 @@ void wifiInit()
 
     indexWait++;
     indexDot++;
-    if(WiFi.status() == WL_CONNECTED){
-      Serial.println("Connected");
+
+    if(WiFi.status() == WL_CONNECTED)
+    {
       break;
     }
 
-  } while (WiFi.status() != WL_CONNECTED || indexWait < 30);
+  } while (indexWait < 30);
+
+  indexWait = 0;
 
   if (WiFi.status() == WL_CONNECTED)
   {
     myDisplay.clear();
-
     myDisplay.drawString(64, 32, "WOW, you are connected!");
     myDisplay.display();
+
     delay(1000);
+
     localIP = WiFi.localIP();
+    remotePort = Udp.remotePort();
+
     myDisplay.clear();
-    myDisplay.drawString(64, 32, "Lokale IP-Adresse:\n" + String(localIP));
+    myDisplay.drawString(64, 10, "Lokale IP-Adresse:\n" + String(localIP)+ "\nUDP-Port:\n" + remotePort);
+    Serial.print("Lokale IP-Adresse: " + String(localIP));
+    Serial.print("UDP-Port: " + remotePort);
     myDisplay.display();
-    //IP-Adresse anzeigen und UDP-Port des geräts
-    // Serial und display
+
+    delay(3000);
+
+    myDisplay.clear();
+    
+
   }
   else
   {
     myDisplay.clear();
     myDisplay.drawString(64, 32, "No connection :(\n WiFi Err-" + String(WiFi.status()));
     myDisplay.display();
+
+    delay(3000);
+
+    myDisplay.clear();
+
   }
 }
